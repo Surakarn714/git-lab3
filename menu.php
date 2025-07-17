cec590377f2eecc661c9b752bdb4d3dae1c63524
Surakarn <6611130037@mut.ac.th>
2025-07-17 16:32:50 +0700

    commit3

--- menu.php
+++ menu.php
@@ -1,11 +1,5 @@
 <?php
     echo "Home";
     echo "Product";
-<<<<<<< HEAD
     echo "Mahanakorn";
 ?>
-=======
-    echo "Map";
-    echo "About me";
-?>
->>>>>>> f81a1b2ff6f56ab3b9e04f9033c03a33ec15fe25
